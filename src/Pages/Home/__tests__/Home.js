14c6b02b59d9d98163c18065001b1541d33aada6
Joseph Casey <me@josephmcasey.com>
2018-04-21 20:40:30 -0700

    Added missing dependencies. Removed test files until proper testing is written.

--- src/Pages/Home/__tests__/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Home from '../Home';
-
-it('renders welcome message', () => {
-  const wrapper = shallow(<Home />);
-  const welcome = <h2>Welcome to React</h2>;
-  expect(wrapper).toContainReact(welcome);
-});
